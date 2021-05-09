983f887ee037767054f4e16441fe65656196a5e7
Andreea <andreea.musat0304@yahoo.com>
2021-05-09 23:45:59 +0300

    Pasul 1

--- src/Components/CurrentSituation/CurrentSituation.js
+++ src/Components/CurrentSituation/CurrentSituation.js
@@ -1,31 +1,67 @@
 import React from 'react';
 import Select from 'react-select';
-import Judete from '../../resources/judete.json';
+//import LastDaySituation from '../../resources/last_day.json';
+import axios from "axios";
 
-class CurrentSituation extends React.Component{
+
+class CurrentSituation extends React.Component {
 
     constructor(props) {
         super(props);
-        //console.log(Judete);
-        let options = [];
-        for(let i = 0; i < Judete.length; i++) {
-            console.log(Judete[i]);
-            let nume_judet = Judete[i].nume;
-            let option = {value: nume_judet, label: nume_judet};
-            options.push(option);
+        this.state = {
+            options: [],
+            selected_county_total_cases: 0,
+            county_info: {}
         }
 
-        this.state = {
-           options: options
-        };
+        this.handleChangeEvent = this.handleChangeEvent.bind(this);
+    }
+    componentDidMount() {
+
+        let county_info = {};
+        let options = [];
+
+        axios.get('http://localhost:5000/last_day_situation')
+            .then((response) => {
+                console.log("res", response);
+                let LastDaySituation = response.data;
+
+                for (let i = 0; i < LastDaySituation.county_data.length; i++) {
+                    let j = LastDaySituation.county_data[i];
+                    let judet = j.county_name;
+
+                    console.log("aaaa", j);
+                    console.log("bbbbbbbbbb", judet);
+
+                    options[i] = {value: judet, label: judet};
+
+                    county_info[judet] = j.total_cases;
+
+                }
+
+                this.setState({
+                    options: options,
+                    county_info: county_info
+                });
+            });
     }
 
+    handleChangeEvent(event) {
+        console.log(this.state);
 
-    render()
-    {
-        return(
+        this.setState({
+            selected_county_info: this.state.county_info[event.value]
+        })
+    }
+
+    render() {
+        return (
             <div>
-                <Select options={this.state.options}/>
+                <Select options={this.state.options} onChange={this.handleChangeEvent}/>
+
+                <div id="total_cases">
+                    {this.state.selected_county_info}
+                </div>
             </div>
         );
 
